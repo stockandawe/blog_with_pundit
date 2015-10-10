1b2e42b55b22609dabd32f83e24182e6b30c0504
Rutul Dave <rutul98@yahoo.com>
2015-10-09 18:12:11 -0700

    first commit: A bare bones blog app with authentication with Pundit and roles with Devise

---
+++ app/policies/post_policy.rb
@@ -0,0 +1,28 @@
+class PostPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  def create?
+    user.present?
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    return true if user.present? && user.admin?
+    user.present? && user == post.user
+  end
+
+  def destroy?
+    user.present? && user.admin?
+  end
+
+  private
+
+  def post
+    record
+  end
+end
